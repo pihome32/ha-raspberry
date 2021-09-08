01ea42b48e5bb953a6eb811d023235145c5f9b29
Demo User <debian@beaglebone.localdomain>
2021-09-08 18:43:57 +0000

    add mqtt

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 
 
-echo none > /sys/class/leds/beaglebone\:green\:usr0/trigger
-echo none > /sys/class/leds/beaglebone\:green\:usr1/trigger
-echo none > /sys/class/leds/beaglebone\:green\:usr2/trigger
-echo none > /sys/class/leds/beaglebone\:green\:usr3/trigger
+
 
 wget https://download.tinkerforge.com/apt/$(lsb_release -is | tr [A-Z] [a-z])/archive.key -q -O - | sudo apt-key add -
 sudo sh -c "echo 'deb https://download.tinkerforge.com/apt/$(lsb_release -is | tr [A-Z] [a-z]) $(lsb_release -cs) main' > /etc/apt/sources.list.d/tinkerforge.list"
 sudo apt update
 sudo apt install brickd
+
+sudo apt install tinkerforge-mqtt
--- sensor.py
+++ sensor.py
@@ -1,79 +1,100 @@
-#!/usr/bin/env python
+
+#!/usr/bin/python3
+
 # -*- coding: utf-8 -*-
 from paho.mqtt import client as mqtt_client
 import time
 import json
+import logging as log
+log.basicConfig(level=log.INFO)
 
-mqtt_broker="10.70.1.95"
-mqtt_port=1883
-mqtt_client_id="tinkerforge_living"
-
-
-HOST = "localhost"
-PORT = 4223
-AQ_UID = "GKX" # Change XYZ to the UID of your Air Quality Bricklet
-MASTER_UID = "6e7cYn"
 
 from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_air_quality import BrickletAirQuality
 from tinkerforge.brick_master import BrickMaster
 
 
-def connect_mqtt():
-	def on_connect(client, userdata, flags, rc):
-		if rc == 0:
-			print("Connected to MQTT Broker!")
-		else:
-			print("Failed to connect, return code %d\n", rc)
-    # Set Connecting Client ID
-	client = mqtt_client.Client(mqtt_client_id)
-	client.username_pw_set("mqtt", "cairnsD18")
-	client.on_connect = on_connect
-	client.connect(mqtt_broker, mqtt_port)
-	return client
-
-def publish(client,topic,msg):
-	msg_count = 0
-	while True:
-		time.sleep(1)
-		result = client.publish(topic, msg,retain=True)
-        # result: [0, 1]
-		status = result[0]
-
-		msg_count += 1
-# Callback function for all values callback
-def cb_all_values(iaq_index, iaq_index_accuracy, temperature, humidity, air_pressure):
+class bbTinkerforge:
+	mqtt_broker="10.70.1.95"
+	mqtt_port=1883
+	mqtt_client_id="tinkerforge_living"
 
 
-	data = {'temp': temp, 'humidity': humidity/100.0, 'iaq_index':iaq_index,'iaq_index_accuracy':iaq_index_accuracy}
-	data_out=json.dumps(data)
+	HOST = "localhost"
+	PORT = 4223
+	AQ_UID = "GKX" # Change XYZ to the UID of your Air Quality Bricklet
+	MASTER_UID = "6e7cYn"
+	ipcon = None
 	
-	publish(client,"ha/sensor/living_room/air_quality/state",data_out)
-	
+	def __init__(self):
+
+		self.ipcon = IPConnection()
+		while True:
+			try:
+				self.ipcon.connect(bbTinkerforge.HOST, bbTinkerforge.PORT)
+				break
+			except Error as e:
+				log.error('Connection Error: ' + str(e.description))
+				time.sleep(1)
+			except socket.error as e:
+				log.error('Socket error: ' + str(e))
+				time.sleep(1)
+		self.ipcon.register_callback(IPConnection.CALLBACK_ENUMERATE, self.cb_enumerate)
+		self.ipcon.register_callback(IPConnection.CALLBACK_CONNECTED, self.cb_connected)
+
+		while True:
+			try:
+				self.ipcon.enumerate()
+				break
+			except Error as e:
+				print(e.description)
+				log.error('Enumerate Error: ' + str(e.description))
+				time.sleep(1)
+
+	def cb_air_quality(self, iaq_index, iaq_index_accuracy, temperature, humidity, air_pressure):
+		if self.aq is not None:
+			print(iaq_index)
+			data = {'temperature': temperature/100.0, 'humidity': humidity/100.0, 'iaq_index':iaq_index,'iaq_index_accuracy':iaq_index_accuracy}
+			data_out=json.dumps(data)
+			#client=mqtt_client.Client(mqtt_client_id)
+			#client.username_pw_set("mqtt", "cairnsD18")
+			#client.connect(mqtt_broker, mqtt_port)
+			
+			#client.publish("ha/sensor/living_room/air_quality/state",data_out)
+			#client.disconnect
+
+
+	def cb_enumerate(self, uid, connected_uid, position, hardware_version,firmware_version, device_identifier, enumeration_type):
+		if enumeration_type == IPConnection.ENUMERATION_TYPE_CONNECTED or enumeration_type == IPConnection.ENUMERATION_TYPE_AVAILABLE:
+			print("wenu")
+			if device_identifier == BrickletAirQuality.DEVICE_IDENTIFIER:
+				try:
+					self.aq = BrickletAirQuality(uid, self.ipcon)
+					self.aq.set_status_led_config(0)
+					self.aq.set_temperature_offset(150)
+					self.aq.register_callback(self.aq.CALLBACK_ALL_VALUES, self.cb_air_quality)
+					self.aq.set_all_values_callback_configuration(1000, False)
+					log.info('Air quality initialised')
+				except Error as e:
+					log.error('Air quality init failed: ' + str(e.description))
+					self.aq = None
+
+	def cb_connected(self, connected_reason):
+		if connected_reason == IPConnection.CONNECT_REASON_AUTO_RECONNECT:
+			log.info('Auto Reconnect')
+
+			while True:
+				try:
+					self.ipcon.enumerate()
+					break
+				except Error as e:
+					log.error('Enumerate Error: ' + str(e.description))
+					time.sleep(1)
+
+
+	#ipcon.disconnect()
 if __name__ == "__main__":
-	ipcon = IPConnection() # Create IP connection
-	 # Create device object
-	master = BrickMaster(MASTER_UID, ipcon)
-	ipcon.connect(HOST, PORT) 
-	master.disable_status_led()# Connect to brickd
-    # Don't use device before ipcon is connected
-	ipcon.disconnect()
-	time.sleep(1)
-	ipcon = IPConnection()
-	aq = BrickletAirQuality(AQ_UID, ipcon)
+	bb_tinkerforge = bbTinkerforge()
+	if bb_tinkerforge.ipcon != None:
+		bb_tinkerforge.ipcon.disconnect()
 	
-	ipcon.connect(HOST, PORT)
-	aq.set_status_led_config(0)
-	aq.set_temperature_offset(150)
-	
-	client = connect_mqtt()
-	client.loop_start()
-	publish(client,"ha/sensor/living_room/air_quality/available","online")
-    # Register all values callback to function cb_all_values
-	aq.register_callback(aq.CALLBACK_ALL_VALUES, cb_all_values)
-
-    # Set period for all values callback to 1s (1000ms)
-	aq.set_all_values_callback_configuration(1000, False)
-
-	input("Press key to exit\n") # Use raw_input() in Python 2
-	ipcon.disconnect()
