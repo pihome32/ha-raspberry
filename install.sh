83579e9fe3b1ad33e811a3b23a665413a2b384a4
pihome32 <39563875+pihome32@users.noreply.github.com>
2021-09-08 20:49:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,10 @@ sudo sh -c "echo 'deb https://download.tinkerforge.com/apt/$(lsb_release -is | t
 sudo apt update
 sudo apt install brickd
 
-<<<<<<< HEAD
 sudo apt install tinkerforge-mqtt
-=======
+
 sudo apt install python3-pip
 pip3 install json
 pip3 install paho-mqtt
 sudo apt install python3-tinkerforge
->>>>>>> 5b98d8e90f6b972921b860db322873395b4eeb22
+
